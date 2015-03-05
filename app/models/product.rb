3d734ca83ab7acbba873dbb19c621e9e08634ef1
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-05 21:15:00 +0100

    Product - add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :description, :title, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
 end
